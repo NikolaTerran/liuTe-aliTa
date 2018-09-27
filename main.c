16ff1fdcbbbd27ef6e21443a8ed5362edebd4881
tianrunliu <tliu3@stuy.edu>
2018-09-27 19:51:46 -0400

    Please read: I uploaded the main.c as the template, we should work on individual functions and put them into main.c, I will start with strchr and strcat first.

---
+++ main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* add function headers below */
+
+int main(){
+
+/* demo strlen */
+
+/* demo strcpy */
+
+/* demo strcat */
+
+/* demo strcmp */
+
+/* demo strchr */
+
+/*Extra************************************************************/
+
+/* demo strstr */
+
+/* demo strncpy */
+
+/* demo strncat */
+
+return 0;
+
+}
